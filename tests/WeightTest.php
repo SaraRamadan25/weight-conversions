f25a9f145472306af408fd8cbaba14de31f4ac77
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-07-25 19:45:39 +0300

    WIP

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -8,8 +8,4 @@ it('can convert kilograms to pounds', function () {
     expect($pounds)->toBe(220.46);
 });
 
-it('can convert kilograms to stones correctly', function () {
-    $pounds = Weight::kilograms(100)->toStones();
 
-    expect($pounds)->toBe(15.747);
-});
