02e31396e23774258c91218ac3f018c190a934a6
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-07-25 18:50:39 +0300

    add toStones

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -7,3 +7,9 @@ it('can convert kilograms to pounds', function () {
 
     expect($pounds)->toBe(220.46);
 });
+
+it('can convert kilograms to stones correctly', function () {
+    $pounds = Weight::kilograms(100)->toStones();
+
+    expect($pounds)->toBe(15.747);
+});
